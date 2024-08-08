7cebd6ffa5e9012dc8acff5d0f42e9b622612059
TrImEaS <trimeas@hotmail.com>
2024-08-08 14:42:36 -0300

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -86,35 +86,41 @@ export default function Footer() {
     <section className="relative flex bg-blue-600 flex-col max-sm:px-5 justify-center items-center gap-3 w-full text-white py-5">
       <div className="flex gap-3 z-10 max-[780px]:flex-col">
         <section className="flex flex-col justify-center w-full gap-3">
-          <a href="https://www.google.com/maps/place/Piedra+Buena+6059,+B1757BRU+Gregorio+de+Laferrere,+Provincia+de+Buenos+Aires/@-34.7479216,-58.5855061,17z/data=!4m6!3m5!1s0x95bcc5a4fefa92a9:0x99f7dbdacc71f017!8m2!3d-34.7479303!4d-58.585655!16s%2Fg%2F11fz963qk0?entry=ttu" className="flex hover:scale-105 duration-300 items-center max-w-[400px] w-full p-4 rounded-lg shadow-lg border-2 gap-2">
+          <a target="_blank" href="https://www.google.com/maps/place/Piedra+Buena+6059,+B1757BRU+Gregorio+de+Laferrere,+Provincia+de+Buenos+Aires/@-34.7479216,-58.5855061,17z/data=!4m6!3m5!1s0x95bcc5a4fefa92a9:0x99f7dbdacc71f017!8m2!3d-34.7479303!4d-58.585655!16s%2Fg%2F11fz963qk0?entry=ttu" className="flex hover:scale-105 duration-300 items-center max-w-[400px] w-full p-4 rounded-lg shadow-lg border-2 gap-2">
             <img aria-hidden="true" alt="location icon" src={mobile_icon} className="w-14 h-14" />
             <div className="flex-1">
               <h2 className="text-lg font-bold text-white">PIEDRA BUENA 6059, GREGORIO DE LAFERRERE</h2>
             </div>
           </a>
-          <a href="https://www.google.com/maps/place/Victorino+de+la+Pl.+1095,+B1755ACU+Rafael+Castillo,+Provincia+de+Buenos+Aires/@-34.7002434,-58.6314094,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcc6c3a62c3bb3:0x9554ebe49da62724!8m2!3d-34.7002479!4d-58.6265385!16s%2Fg%2F11h553td5f?entry=ttu" className="flex hover:scale-105 duration-300 items-center max-w-[400px] w-full p-4 rounded-lg shadow-lg border-2 gap-2">
+          <a target="_blank" href="https://www.google.com/maps/place/Victorino+de+la+Pl.+1095,+B1755ACU+Rafael+Castillo,+Provincia+de+Buenos+Aires/@-34.7002434,-58.6314094,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcc6c3a62c3bb3:0x9554ebe49da62724!8m2!3d-34.7002479!4d-58.6265385!16s%2Fg%2F11h553td5f?entry=ttu" className="flex hover:scale-105 duration-300 items-center max-w-[400px] w-full p-4 rounded-lg shadow-lg border-2 gap-2">
             <img aria-hidden="true" alt="location icon" src={mobile_icon} className="w-14 h-14" />
             <div className="flex-1">
               <h2 className="text-lg font-bold text-white">VICTORINO DE LA PLAZA 1095, RAFAEL CASTILLO</h2>
             </div>
           </a>
-          <a href="https://www.google.com/maps/place/Buenos+Aires+Central+Market/@-34.7089458,-58.5030919,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcceddac97550b:0x7d8fff4e7986b7d9!8m2!3d-34.7089502!4d-58.500517!16s%2Fg%2F120j3kz8?entry=ttu" className="flex hover:scale-105 duration-300 items-center max-w-[400px] w-full p-4 rounded-lg shadow-lg border-2 gap-2">
+          <a target="_blank" href="https://www.google.com/maps/place/Outlet+Panificados+%26+Golosinas/@-34.7134188,-58.4955985,20.75z/data=!4m14!1m7!3m6!1s0x95bcceddac97550b:0x7d8fff4e7986b7d9!2sBuenos+Aires+Central+Market!8m2!3d-34.7089502!4d-58.500517!16s%2Fg%2F120j3kz8!3m5!1s0x95bccf0be3a590ef:0x53f281e80b285e5f!8m2!3d-34.7133725!4d-58.4952592!16s%2Fg%2F11t29q39g4?entry=ttu" className="flex hover:scale-105 duration-300 items-center max-w-[400px] w-full p-4 rounded-lg shadow-lg border-2 gap-2">
             <img aria-hidden="true" alt="location icon" src={mobile_icon} className="w-14 h-14" />
             <div className="flex-1">
               <h2 className="text-lg font-bold text-white">MERCADO CENTRAL LOCAL 61 Y 123, TAPIALES</h2>
             </div>
           </a>
+          <a target="_blank" href="https://www.google.com/maps/place/Guillermo+Marconi+3496,+B1765LEL+Isidro+Casanova,+Provincia+de+Buenos+Aires/@-34.7125326,-58.5959213,17z/data=!3m1!4b1!4m5!3m4!1s0x95bcc67bf13fa3b7:0xcb6e74c8058a287!8m2!3d-34.712537!4d-58.5933464?entry=ttu" className="flex hover:scale-105 duration-300 items-center max-w-[400px] w-full p-4 rounded-lg shadow-lg border-2 gap-2">
+            <img aria-hidden="true" alt="location icon" src={mobile_icon} className="w-14 h-14" />
+            <div className="flex-1">
+              <h2 className="text-lg font-bold text-white">GUILLERMO MARCONI 3496, ISIDRO CASANOVA</h2>
+            </div>
+          </a>
         </section>
 
         <section className="flex flex-col justify-center w-full gap-3">
-          <a href="https://wa.me/+5491157999632?text=Hola me comunico desde la pagina de OutletGolosinas." className="flex duration-300 hover:scale-105 items-center max-w-[400px] w-full gap-2 p-4 rounded-lg shadow-lg border-2">
+          <a target="_blank" href="https://wa.me/+5491157999632?text=Hola me comunico desde la pagina de OutletGolosinas." className="flex duration-300 hover:scale-105 items-center max-w-[400px] w-full gap-2 p-4 rounded-lg shadow-lg border-2">
             <FaWhatsapp className="text-green-500 text-5xl font-bold"/>
             <div className="flex-1">
               <h2 className="text-lg font-bold">GREGORIO DE LAFERRE</h2>
               <p className="text-xl">11-5799-9632</p>
             </div>
           </a>
-          <a href="https://wa.me/+5491164292377?text=Hola me comunico desde la pagina de OutletGolosinas." className="flex duration-300 hover:scale-105 items-center max-w-[400px] w-full gap-2 p-4 rounded-lg shadow-lg border-2">
+          <a target="_blank" href="https://wa.me/+5491164292377?text=Hola me comunico desde la pagina de OutletGolosinas." className="flex duration-300 hover:scale-105 items-center max-w-[400px] w-full gap-2 p-4 rounded-lg shadow-lg border-2">
             <FaWhatsapp className="text-green-500 text-5xl font-bold"/>
             <div className="flex-1">
               <h2 className="text-lg font-bold">RAFAEL CASTILLO</h2>
@@ -125,9 +131,9 @@ export default function Footer() {
             <FaWhatsapp className="text-green-500 text-5xl font-bold"/>
             <div className="flex-1 flex flex-wrap gap-x-3">
               <h2 className="text-lg font-bold w-full">MERCADO CENTRAL</h2>
-              <a href="https://wa.me/+5491126180413?text=Hola me comunico desde la pagina de OutletGolosinas." className="text-xl">11-2618-0413</a>
+              <a target="_blank" href="https://wa.me/+5491126180413?text=Hola me comunico desde la pagina de OutletGolosinas." className="text-xl">11-2618-0413</a>
               <span className="max-md:hidden">/</span>
-              <a href="https://wa.me/+5491157092539?text=Hola me comunico desde la pagina de OutletGolosinas." className="text-xl">11-5709-2539</a>
+              <a target="_blank" href="https://wa.me/+5491157092539?text=Hola me comunico desde la pagina de OutletGolosinas." className="text-xl">11-5709-2539</a>
             </div>
           </article>
         </section>
